a061f298325e13f8547e65117ab4594c5679d4ac
anwardev <anwarali123ldh@gmail.com>
2022-09-07 21:32:40 +0530

    first commit

---
+++ css/var veriable = 1234.js
@@ -0,0 +1,7 @@
+var veriable = 1234
+var boolean = true || false
+var string = '1234'
+var array = []
+var object = {name:'anwar',phone:'sdsdsd'}
+var float = 1.1
+var nulla = null
---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+function openNav() {
+  document.getElementById("sidemenu").style.width = "200px";
+}
+
+function closeNav() {
+  document.getElementById("sidemenu").style.width = "0";
+}
